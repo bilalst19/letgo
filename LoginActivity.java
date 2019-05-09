01f76da4f99c1c3e554bb656a0cacefa247f5278
bilalst19 <45921822+bilalst19@users.noreply.github.com>
2019-05-09 14:00:20 +0300

    Add files via upload
    2.versiyon

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,100 +1,13 @@
 package com.example.a90535.letgo;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import org.w3c.dom.Text;
-
-public class LoginActivity extends Activity implements View.OnClickListener {
-
-    private Button buttonGirisYap;
-    private EditText editTextMail;
-    private EditText editTextSifre;
-    private TextView textViewGiris;
-    private ProgressDialog progressDialog;
-    private FirebaseAuth firebaseAuth;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        firebaseAuth=FirebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser() != null)
-        {
-            finish();
-            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-        }
-
-
-
-        editTextMail = (EditText) findViewById(R.id.girisMailEditText);
-        editTextSifre = (EditText) findViewById(R.id.girisSifreEditText);
-        buttonGirisYap= (Button) findViewById(R.id.girisButon);
-        textViewGiris= (TextView) findViewById(R.id.textView);
-        progressDialog=new ProgressDialog(this);
-
-        buttonGirisYap.setOnClickListener(this);
-        textViewGiris.setOnClickListener(this);
-    }
-    private void userLogin(){
-        String email=editTextMail.getText().toString().trim();
-        String sifre=editTextSifre.getText().toString().trim();
-        if (TextUtils.isEmpty(email))
-        {
-            //mail bos
-            Toast.makeText(this,"Lütfen geçerli bir mail adresi girin",Toast.LENGTH_SHORT).show();
-        }
-        if (TextUtils.isEmpty(sifre))
-        {
-            //sifre bos
-            Toast.makeText(this,"Lütfen bir şifre girin",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        progressDialog.setMessage("Giris Yapılıyor ...");
-        progressDialog.show();
-
-        firebaseAuth.signInWithEmailAndPassword(email,sifre)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.dismiss();
-
-                        if(task.isSuccessful())
-                        {
-                            finish();
-                            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v==buttonGirisYap)
-        {
-            userLogin();
-        }
-        if(v==textViewGiris)
-        {
-
-            startActivity(new Intent(this,MainActivity.class));
-        }
-
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -20,13 +20,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity  {
 
-    private Button kayitSifreButon;
-    private EditText kayitEditText;
-    private EditText kayitSifreEditText;
-    private TextView textViewKayitOl;
-    private ProgressDialog progressDialog;
+    private Button kayitbtn,girisbtn;
+
+
 
     private FirebaseAuth firebaseAuth;
 
@@ -35,75 +33,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser() != null)
-        {
-            finish();
-            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-        }
+        kayitbtn = (Button) findViewById(R.id.kayitbtn);
+        girisbtn=(Button) findViewById(R.id.girisbtn);
 
-        progressDialog = new ProgressDialog(this);
-
-        kayitSifreButon = (Button) findViewById(R.id.kayitSifreButon);
-
-        kayitEditText= (EditText) findViewById(R.id.kayitEditText);
-        kayitSifreEditText= (EditText) findViewById(R.id.kayitSifreEditText);
-        textViewKayitOl= (TextView) findViewById(R.id.textViewKayitOl);
-
-        kayitSifreButon.setOnClickListener(this);
-        textViewKayitOl.setOnClickListener(this);
-    }
-    private void registerUser(){
-        String email = kayitEditText.getText().toString().trim();
-        String password = kayitSifreEditText.getText().toString().trim();
-
-        if (TextUtils.isEmpty(email))
-        {
-            //mail bos
-            Toast.makeText(this,"Lütfen geçerli bir mail adresi girin",Toast.LENGTH_SHORT).show();
-        }
-        if (TextUtils.isEmpty(password))
-        {
-            //sifre bos
-            Toast.makeText(this,"Lütfen bir şifre girin",Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        //doğrulama olunca
-        progressDialog.setMessage("Kayıt Olunuyor ...");
-        progressDialog.show();
-
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful())
-                        {
-                            //kayit basarlı
-
-                            Toast.makeText(MainActivity.this,"Başarıyla Kayıt Olundu",Toast.LENGTH_SHORT).show();
-                            progressDialog.hide();
-                            finish();
-                            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-                        }else {
-                            Toast.makeText(MainActivity.this,"Kayıt Olma Başarısız Tekrar Deneyin",Toast.LENGTH_SHORT).show();
-                            progressDialog.dismiss();
-                            finish();
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view==kayitSifreButon)
-        {
-            registerUser();
-        }
-        if (view==textViewKayitOl)
-        {
-            startActivity(new Intent(this,LoginActivity.class));
-        }
 
     }
 }
